b00ca7b4ce96234ce2ce19ebe6e548470acad82a
animals4all <animals4all@gmail.com>
2015-06-27 16:10:26 -0400

    Final Calculator

--- calculator.py
+++ calculator.py
@@ -8,12 +8,16 @@ from arithmetic import *
 
 def main():
 	# Your code goes here
+	errorMsg = "I don't know what that means."
 	quit = False
 	while not quit:
 		command = raw_input().split()
 		if command[0] in ("+", "-", "*", "/", "pow", "mod"):
 			try:
 				num1, num2 = int(command[1]), int(command[2])
+			except ValueError:
+				print errorMsg
+			else:
 				if command[0] == "+":
 					print add(num1, num2)
 				elif command[0] == "-":
@@ -26,21 +30,23 @@ def main():
 					print power(num1, num2)
 				elif command[0] == "mod":
 					print mod(num1, num2)
-			except ValueError:
-				print "I don't know what that means."
+					
 		elif command[0] in ("square", "cube"):
 			try:
 				num1 = int(command[1])
+			except ValueError:
+				print errorMsg
+			else:
 				if command[0] == "square":
 					print square(num1)
 				elif command[0] == "cube":
 					print cube(num1)
-			except ValueError:
-				print "I don't know what that means."
+					
 		elif command[0] == "q":
 			quit = True
+			
 		else:
-			print "I don't know what that means."
+			print errorMsg
 
 if __name__ == '__main__':
     main()
